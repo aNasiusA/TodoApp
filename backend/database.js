557662f22ddac93695ade692ee0d6ba46456da34
aNasiusA <141585116+aNasiusA@users.noreply.github.com>
2024-12-15 22:54:28 +0000

    Connected To remotely hosted DB

--- backend/database.js
+++ backend/database.js
@@ -1,11 +1,19 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
+import fs from "fs";
+dotenv.config();
 
 const pool = mysql
   .createPool({
-    host: "localhost",
-    user: "root",
-    password: "1234",
-    database: "todo_app_simple",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    ssl: {
+      ca: fs.readFileSync(process.env.CERT_PATH),
+    },
+    connectionLimit: 10,
   })
   .promise();
 
